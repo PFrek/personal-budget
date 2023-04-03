aba6c6db950d88deba87fed336d55d68b6d2d9d8
Pedro Favoreto Gaya <pedrogayaf@gmail.com>
2023-04-03 14:43:33 -0300

    Add functionality to create envelopes

--- models/envelope.js
+++ models/envelope.js
@@ -5,12 +5,44 @@ const envelopes = [
     budget: 100,
   }
 ];
+let nextId = envelopes[envelopes.length-1].id+1;
 
 const getAll = () => {
   return envelopes;
 };
 
+const findId = (id) => {
+  const envelope = envelopes.find((elem) => {
+    return elem.id == id;
+  });
+
+  return envelope;
+};
+
+const createEnvelope = (envelope) => {
+  if(!envelope) {
+    throw new Error("Envelope must not be empty.");
+  }
+
+  const title = envelope.title;
+  if(!title || typeof(title) !== "string") {
+    throw new Error("Title of envelope must be a valid string.");
+  }
+
+  const budget = envelope.budget;
+  if(!budget || typeof(budget) !== "number") {
+    throw new Error("Budget of envelope must be a valid number.");
+  }
+
+  envelope.id = nextId++;
+
+  envelopes.push(envelope);
+
+  return findId(envelope.id);
+};
 
 module.exports = {
   getAll,
+  createEnvelope,
+  findId,
 };
--- routes/envelopes.js
+++ routes/envelopes.js
@@ -10,4 +10,32 @@ envelopesRouter.get('/', (req, res, next) => {
 });
 
 
+// Validate request body envelope
+const validateBodyEnvelope = (req, res, next) => {
+  const envelope = req.body.envelope;
+
+  if(!envelope) {
+    return next(new Error("Request body must contain valid envelope object."));
+  }
+
+  const title = envelope.title;
+  if(!title || typeof(title) !== "string") {
+    return next(new Error("Request envelope must contain a valid title."));
+  }
+
+  const budget = envelope.budget;
+  if(!budget || typeof(budget) !== "number") {
+    return next(new Error("Request envelope must contain a valid budget."));
+  }
+
+  next();
+};
+
+// Create a new envelope
+envelopesRouter.post('/', validateBodyEnvelope, (req, res, next) => {
+  const createdEnvelope = Envelope.createEnvelope(req.body.envelope);
+
+  res.status(201).send({ envelope: createdEnvelope });
+});
+
 module.exports = { envelopesRouter };
