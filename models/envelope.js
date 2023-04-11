8e74f6319dab0681d718e9e67d6dca4a28ed8ae3
Pedro Favoreto Gaya <pedrogayaf@gmail.com>
2023-04-11 09:04:52 -0300

    Create transactions table in DB

--- models/envelope.js
+++ models/envelope.js
@@ -1,4 +1,6 @@
 const Pool = require('pg').Pool;
+
+console.log("Initializing Database");
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -6,6 +8,35 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: 5432
 });
+
+pool.query(
+  `CREATE TABLE IF NOT EXISTS envelopes (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    budget money NOT NULL
+  );`,
+  (error) => {
+    if(error) {
+      throw error;
+    }
+    console.log('Envelopes table initialized');
+});
+
+pool.query(
+  `CREATE TABLE IF NOT EXISTS transactions (
+    id SERIAL PRIMARY KEY,
+    date date NOT NULL,
+    amount money NOT NULL,
+    recipient_id INTEGER REFERENCES envelopes(id),
+    payer_id INTEGER REFERENCES envelopes(id)
+  );`,
+  (error) => {
+    if(error) {
+      throw error;
+    }
+    console.log('Transactions table initialized');
+});
+console.log("Database initialized");
 
 const getAll = () => {
   return pool.query('SELECT * FROM envelopes ORDER BY id ASC')
